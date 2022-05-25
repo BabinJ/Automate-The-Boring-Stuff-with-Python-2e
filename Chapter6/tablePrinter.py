48c2e51a245772bde719cb1813a5c3cbe926d1a7
Josh Babin <josh.babin@gmail.com>
2022-05-25 14:17:50 -0500

    tablePrinter practice problem in progress. Lists print out justified, but not in a table format yet.

---
+++ Chapter6/tablePrinter.py
@@ -0,0 +1,31 @@
+# Table Printer
+
+# tablePrinter.py - Write a function named printTable() that takes a list of lists of strings and displays it in a well-organized
+# table with each column right-justified. Assume that all the inner lists will contain the same number of strings
+
+# Define the printTable() function to do the things
+
+
+tableData = [['apples','oranges','cherries','banana'],
+             ['Alice','Bob','Carol','David'],
+             ['dogs','cats','moose','goose']]
+
+def printTable(table):
+    # Create am empty list of columnwidths
+    colWidths = [0] * len(table)
+
+    # Populate the columnwidths by looping through tableData
+    for col in range(len(colWidths)):
+        maxLen = 0
+        for i in table[col]:
+            if len(i) > maxLen:
+                maxLen = len(i)
+        colWidths[col] = maxLen
+
+    # Print out the table
+    for sublist in range(len(table)):
+        for i in table[sublist]:
+            print(i.rjust(colWidths[sublist]), " ")
+
+output = printTable(tableData)
+print(output)
